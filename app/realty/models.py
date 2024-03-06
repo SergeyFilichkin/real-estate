037078f794f17453a79af1bd8de2f27097535fb5
Ivan1586 <154088646+Ivan1586@users.noreply.github.com>
2024-03-06 13:45:24 +0400

    M-4: Добавил модель квартиры на сайт
    * M-4: Добавил модель квартиры на сайт
    
    * M-4: Добавил поле status в модель
    
    * M-4: Изменил наименования
    
    * M-4: Изменил max_length и добавил имя для поля
    
    * M-4: Сделал миграции
    
    ---------
    
    Co-authored-by: Ivan Rodionov <ivan.rodionov1586@gmail.com>

--- app/realty/models.py
+++ app/realty/models.py
@@ -1,2 +1,20 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
+
+class Flat(models.Model):
+
+    STATUS_CHOICES = [
+        ('On sale', 'В продаже'),
+        ('Sold', 'Продана')
+    ]
+
+    square = models.FloatField(verbose_name='Общая площадь', validators=[MinValueValidator(limit_value=0)])
+    living_space = models.FloatField(verbose_name='Жилая площадь', validators=[MinValueValidator(limit_value=0)])
+    kitchen_area = models.FloatField(verbose_name='Площадь кухни', validators=[MinValueValidator(limit_value=0)])
+    rooms = models.PositiveSmallIntegerField(verbose_name='Количество комнат')
+    floor = models.PositiveSmallIntegerField(verbose_name='Этаж')
+    status = models.CharField(max_length=7, choices=STATUS_CHOICES, default='On sale', verbose_name='Статус')
+    price = models.PositiveBigIntegerField(verbose_name='Цена')
+    description = models.TextField(verbose_name='Описание')
+    photo = models.ImageField()
