4b3185740831560ee08b7e823780d818dabd6bcd
Ivan Rodionov <ivan.rodionov1586@gmail.com>
2024-03-06 13:32:56 +0400

    M-4: Изменил наименования

--- app/realty/models.py
+++ app/realty/models.py
@@ -5,8 +5,8 @@ from django.db import models
 class Flat(models.Model):
 
     STATUS_CHOICES = [
-        ('OS', 'В продаже'),
-        ('S', 'Продана')
+        ('On sale', 'В продаже'),
+        ('Sold', 'Продана')
     ]
 
     square = models.FloatField(verbose_name='Общая площадь', validators=[MinValueValidator(limit_value=0)])
@@ -14,7 +14,7 @@ class Flat(models.Model):
     kitchen_area = models.FloatField(verbose_name='Площадь кухни', validators=[MinValueValidator(limit_value=0)])
     rooms = models.PositiveSmallIntegerField(verbose_name='Количество комнат')
     floor = models.PositiveSmallIntegerField(verbose_name='Этаж')
-    status = models.CharField(max_length=2, choices=STATUS_CHOICES, default='OS')
+    status = models.CharField(max_length=2, choices=STATUS_CHOICES, default='On sale')
     price = models.PositiveBigIntegerField(verbose_name='Цена')
     description = models.TextField(verbose_name='Описание')
     photo = models.ImageField()
