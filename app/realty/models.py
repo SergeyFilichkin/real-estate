19907c08e293bb2573fd1511685a3a34d13cc9cb
Ivan Rodionov <ivan.rodionov1586@gmail.com>
2024-03-06 12:42:01 +0400

    M-4: Добавил модель квартиры на сайт

--- app/realty/models.py
+++ app/realty/models.py
@@ -1,2 +1,13 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
+
+class Flat(models.Model):
+    square = models.FloatField(verbose_name='Общая площадь', validators=[MinValueValidator(limit_value=0)])
+    living_space = models.FloatField(verbose_name='Жилая площадь', validators=[MinValueValidator(limit_value=0)])
+    kitchen_area = models.FloatField(verbose_name='Площадь кухни', validators=[MinValueValidator(limit_value=0)])
+    rooms = models.PositiveSmallIntegerField(verbose_name='Количество комнат')
+    floor = models.PositiveSmallIntegerField(verbose_name='Этаж')
+    price = models.PositiveBigIntegerField(verbose_name='Цена')
+    description = models.TextField(verbose_name='Описание')
+    photo = models.ImageField()
