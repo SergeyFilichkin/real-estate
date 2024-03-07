5a1c23db9574c39404a753627280058c12ab2137
Ivan1586 <154088646+Ivan1586@users.noreply.github.com>
2024-03-07 17:07:41 +0400

    M-6: Добавил swagger в проект
    * M-6: Добавил swagger в проект
    
    * M-6: Убрал drf_spectacular_sidecar
    
    * M-6: Убрал spectacular_settings
    
    ---------
    
    Co-authored-by: Ivan Rodionov <ivan.rodionov1586@gmail.com>

--- app/config/settings.py
+++ app/config/settings.py
@@ -19,7 +19,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'realty.apps.RealtyConfig',
-    'rest_framework'
+    'rest_framework',
+    'drf_spectacular'
 ]
 
 MIDDLEWARE = [
@@ -90,3 +91,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema'
+}
--- app/config/urls.py
+++ app/config/urls.py
@@ -2,12 +2,15 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from config import settings
 
-from realty.views import *
+from realty.views import FlatListView, FlatDetailView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='docs'),
     path('api/flats/', FlatListView.as_view()),
     path('api/flats/<int:flat_id>/', FlatDetailView.as_view())
 ]
