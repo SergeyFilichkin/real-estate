38bd1480b09fbd70d285f8e0ef621334eddc1ab1
Ivan Rodionov <ivan.rodionov1586@gmail.com>
2024-03-07 17:06:30 +0400

    M-6: Убрал spectacular_settings

--- app/config/settings.py
+++ app/config/settings.py
@@ -95,10 +95,3 @@ MEDIA_URL = '/media/'
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema'
 }
-
-SPECTACULAR_SETTINGS = {
-    'TITLE': 'Your Project API',
-    'DESCRIPTION': 'Your project description',
-    'VERSION': '1.0.0',
-    'SERVE_INCLUDE_SCHEMA': False
-}
