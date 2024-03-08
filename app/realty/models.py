b6183241005a189d2652a76c3f64621ebf872e66
Ivan1586 <154088646+Ivan1586@users.noreply.github.com>
2024-03-08 13:47:41 +0400

    M-7: Добавил модель floor (#10)

--- app/realty/models.py
+++ app/realty/models.py
@@ -13,8 +13,13 @@ class Flat(models.Model):
     living_space = models.FloatField(verbose_name='Жилая площадь', validators=[MinValueValidator(limit_value=0)])
     kitchen_area = models.FloatField(verbose_name='Площадь кухни', validators=[MinValueValidator(limit_value=0)])
     rooms = models.PositiveSmallIntegerField(verbose_name='Количество комнат')
-    floor = models.PositiveSmallIntegerField(verbose_name='Этаж')
     status = models.CharField(max_length=7, choices=STATUS_CHOICES, default='On sale', verbose_name='Статус')
     price = models.PositiveBigIntegerField(verbose_name='Цена')
     description = models.TextField(verbose_name='Описание')
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    floor = models.ForeignKey('Floor', on_delete=models.PROTECT, verbose_name='Этаж')
+
+
+class Floor(models.Model):
+    name = models.CharField(max_length=100)
+    number = models.PositiveSmallIntegerField(db_index=True)
