29ad8fb6cf907a6de316b42d5e6cd4f82888d3cc
«Nick-voz» <nik.vozisow@gmail.com>
2024-03-08 14:04:15 +0300

    exception optimization

--- app/realty/views.py
+++ app/realty/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist
 
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -17,7 +18,7 @@ class FlatDetailView(APIView):
     def get(self, request, flat_id):
         try:
             flat = Flat.objects.get(id=flat_id)
-        except:
+        except ObjectDoesNotExist:
             return Response({'error': 'Object does not exists'})
 
         return Response(data=FlatSerializer(flat).data)
