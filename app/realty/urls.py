bdec04c6165aec89ee1b95a7f0e1cc770567937a
Ivan Rodionov <ivan.rodionov1586@gmail.com>
2024-03-11 11:45:58 +0400

    M-8: Исправил апи

--- app/realty/urls.py
+++ app/realty/urls.py
@@ -1,14 +1,10 @@
 from django.conf.urls.static import static
 from django.urls import path
 
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
-
 from config import settings
 from .views import FlatListView, FlatDetailView, FloorListView, FloorDetailView
 
 urlpatterns = [
-    path('schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('schema/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='docs'),
     path('flats/', FlatListView.as_view()),
     path('flats/<int:flat_id>/', FlatDetailView.as_view()),
     path('floors/', FloorListView.as_view()),
