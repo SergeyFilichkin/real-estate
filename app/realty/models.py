a503a1fe139fe158b0489172d3e4a9d7e41be840
Ivan1586 <154088646+Ivan1586@users.noreply.github.com>
2024-03-13 10:16:18 +0400

    M-10: Добавил модель дома
    * M-10: Добавил модель дома
    
    * M-10: Дополнил модель дома
    
    * M-10: Внес правки в модель дома
    
    * M-10: Прописал константы, внес правки в модель Flat
    
    * M-10: Изменил наименования

--- app/realty/models.py
+++ app/realty/models.py
@@ -3,8 +3,10 @@ from django.db import models
 
 
 class Flat(models.Model):
+    ON_SALE = 'On sale'
+    SOLD = 'Sold'
 
-    STATUS_CHOICES = [("On sale", "В продаже"), ("Sold", "Продана")]
+    STATUS_CHOICES = [(ON_SALE, "В продаже"), (SOLD, "Продана")]
 
     square = models.FloatField(
         verbose_name="Общая площадь", validators=[MinValueValidator(limit_value=0)]
@@ -17,13 +19,14 @@ class Flat(models.Model):
     )
     rooms = models.PositiveSmallIntegerField(verbose_name="Количество комнат")
     status = models.CharField(
-        max_length=7, choices=STATUS_CHOICES, default="On sale", verbose_name="Статус"
+        max_length=7, choices=STATUS_CHOICES, default=ON_SALE, verbose_name="Статус"
     )
     price = models.PositiveBigIntegerField(verbose_name="Цена")
     description = models.TextField(verbose_name="Описание")
     photo = models.ImageField(upload_to="photos/%Y/%m/%d/")
     floor = models.ForeignKey("Floor", on_delete=models.PROTECT, verbose_name="Этаж")
     category = models.ForeignKey('FlatCategory', null=True, blank=True, on_delete=models.PROTECT, verbose_name='Класс квартиры')
+    building = models.ForeignKey('Building', null=True, blank=True, on_delete=models.PROTECT)
 
 
 class Floor(models.Model):
@@ -37,4 +40,43 @@ class FlatCategory(models.Model):
         ('Comfort', 'Комфорт'),
         ('Luxe', 'Люкс')
     ]
+
     name = models.CharField(max_length=7, choices=CLASS_CHOICES, db_index=True)
+
+
+class Building(models.Model):
+    STATUS_UNDER_CONSTRUCTION = 'Under construction'
+    STATUS_PASSED = 'Passed'
+
+    STATUS_CHOICES = [
+        (STATUS_UNDER_CONSTRUCTION, 'Строится'),
+        (STATUS_PASSED, 'Сдан')
+    ]
+
+    TYPE_BUILDING_CHOICES = [
+        ('Brick', 'Кирпичный'),
+        ('Monolithic', 'Монолитный'),
+        ('Panel', 'Панельный'),
+    ]
+
+    ZERO_ELEVATORS = 0
+    ONE_ELEVATOR = 1
+    TWO_ELEVATORS = 2
+    THREE_ELEVATORS = 3
+
+    ELEVATORS_CHOICES = [
+        (ZERO_ELEVATORS, 'Ноль'),
+        (ONE_ELEVATOR, 'Один'),
+        (TWO_ELEVATORS, 'Два'),
+        (THREE_ELEVATORS, 'Три')
+    ]
+
+    name = models.CharField(max_length=100)
+    date_of_construction = models.DateField(verbose_name='Дата постройки дома')
+    date_of_delivery = models.DateField(verbose_name='Дата сдачи дома')
+    address = models.CharField(max_length=100, verbose_name='Адрес')
+    number = models.PositiveSmallIntegerField(verbose_name='Номер дома', db_index=True)
+    status = models.CharField(max_length=25, choices=STATUS_CHOICES, default=STATUS_UNDER_CONSTRUCTION, verbose_name='Статус')
+    type = models.CharField(max_length=10, choices=TYPE_BUILDING_CHOICES, null=True, blank=True, verbose_name='Тип дома')
+    has_parking = models.BooleanField(default=True, verbose_name='Паркинг')
+    elevators = models.IntegerField(choices=ELEVATORS_CHOICES, default=TWO_ELEVATORS, verbose_name='Лифты')
