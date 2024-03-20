7cf9c542d999d4e8816abf10959f72ed80d6f4a5
Ivan Rodionov <ivan.rodionov1586@gmail.com>
2024-03-20 10:58:21 +0400

    M-13: Изменил маршруты

--- app/realty/urls.py
+++ app/realty/urls.py
@@ -3,16 +3,16 @@ from django.urls import path, include
 
 from config import settings
 
-from .domain.building.urls import urlpatterns as building_urls
-from .domain.flat.urls import urlpatterns as flat_urls
-from .domain.floor.urls import urlpatterns as floor_urls
-from .domain.project.urls import urlpatterns as project_urls
+flats_api_v1_urls = [path('', include('realty.domain.flat.urls'))]
+buildings_api_v1_urls = [path('', include('realty.domain.building.urls'))]
+floors_api_v1_urls = [path('', include('realty.domain.floor.urls'))]
+projects_api_v1_urls = [path('', include('realty.domain.project.urls'))]
 
 urlpatterns = [
-    path('v1/', include(building_urls)),
-    path('v1/', include(flat_urls)),
-    path('v1/', include(floor_urls)),
-    path('v1/', include(project_urls))
+    path('v1/', include(buildings_api_v1_urls)),
+    path('v1/', include(flats_api_v1_urls)),
+    path('v1/', include(floors_api_v1_urls)),
+    path('v1/', include(projects_api_v1_urls))
 ]
 
 if settings.DEBUG:
