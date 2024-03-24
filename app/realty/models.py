cebb14f1e73de3db2180492d3ecd811f9e8c9b0c
Mark Seleznyov <nebose@mail.ru>
2024-03-25 05:59:58 +0800

    М-4: Добавление модели квартир

--- app/realty/models.py
+++ app/realty/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Flat(models.Model):
+    name = models.CharField(max_length=120)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    overall_square = models.IntegerField()
+    living_square = models.IntegerField()
+    rooms = models.IntegerField()
+    view_from_windows = models.CharField(max_length=120)
+    lavatory = models.IntegerField()
+    level = models.IntegerField()
+    elevator = models.CharField(max_length=120)
+    year_of_sale = models.IntegerField()
+    parking = models.CharField(max_length=50)
+    is_complete = models.BooleanField(default=True)
+    is_kitchen = models.BooleanField()
