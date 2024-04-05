2d5abb9267130d3a6d7bdc9e6fabe99145c02460
Mark Seleznyov <nebose@mail.ru>
2024-04-05 18:08:15 +0700

    М-4: Добавление констант в models.py, добавление в .gitignore файлов __pycache__

--- app/realty/models.py
+++ app/realty/models.py
@@ -2,16 +2,30 @@ from django.db import models
 
 
 class Flat(models.Model):
+    COURTYARD = "courtyard"
+    STREET_OUTSIDE = "street_outside"
+    WINDOW_VIEWS = [
+        (COURTYARD, "внутренний двор"),
+        (STREET_OUTSIDE, "внешняя улица")
+    ]
+    ON_GROUND = "on_ground"
+    UNDERGROUND = "underground"
+
+    PARKING_CHOICES = (
+        (ON_GROUND, "наземный"),
+        (UNDERGROUND, "подземный")
+    )
+
     name = models.CharField(max_length=120)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     overall_square = models.IntegerField()
     living_square = models.IntegerField()
     rooms = models.IntegerField()
-    view_from_windows = models.CharField(max_length=120)
+    view_from_windows = models.CharField(max_length=120, choices=WINDOW_VIEWS, default=COURTYARD, blank=True)
     lavatory = models.IntegerField()
     level = models.IntegerField()
     elevator = models.CharField(max_length=120)
     year_of_sale = models.IntegerField()
-    parking = models.CharField(max_length=50)
+    parking = models.CharField(max_length=50, choices=PARKING_CHOICES, default=ON_GROUND)
     is_complete = models.BooleanField(default=True)
     is_kitchen = models.BooleanField()
