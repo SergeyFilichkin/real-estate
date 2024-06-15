d7fb14c3c5c09dd934b9a6e05514ae9bd0be4d4a
PPanda7 <pashasokolov779@gmail.com>
2024-06-15 16:12:59 +0300

    М-15: Добавил админку для этажей.

--- app/realty/models/floor.py
+++ app/realty/models/floor.py
@@ -1,6 +1,9 @@
-from django.db import models
-
+from django.db import models # type: ignore
 
 class Floor(models.Model):
     name = models.CharField(max_length=100)
     number = models.PositiveSmallIntegerField(db_index=True)
+
+    class Meta:
+        verbose_name = "Этаж"
+        verbose_name_plural = "Этаж"
